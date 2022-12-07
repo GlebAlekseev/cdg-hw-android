190774af1ac797cac7dc9e8d744b0a2eaf105b6b
GlebAlekseev <glebalekseevjk@gmail.com>
2022-12-07 14:16:02 +0300

    task 2

--- app/src/main/java/com/example/premierleaguefixtures/MainActivity.kt
+++ app/src/main/java/com/example/premierleaguefixtures/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.premierleaguefixtures
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null){
+            launchFragment(BlackFragment())
+        }
+    }
+
+    private fun launchFragment(fragment: Fragment) {
+        // supportFragmentManager ссылка на FragmentManager привязанный к данной активити
+        // beginTransaction - используя апи FragmentTransaction фиксируется набор действий по отношению к фрагментам
+        // add - помещает фрагмент в контейнер
+        // commit - добавление фиксации в очередь на исполнение в главном потоке
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_fcv, fragment)
+            .commit()
     }
 }
