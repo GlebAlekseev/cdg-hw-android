57b76b153de1b4279babc71193868057528e8f32
GlebAlekseev <glebalekseevjk@gmail.com>
2023-01-09 23:12:42 +0300

    added room to repository

--- app/src/main/java/com/glebalekseevjk/premierleaguefixtures/data/repository/MatchInfoRepositoryImpl.kt
+++ app/src/main/java/com/glebalekseevjk/premierleaguefixtures/data/repository/MatchInfoRepositoryImpl.kt
@@ -1,57 +1,51 @@
 package com.glebalekseevjk.premierleaguefixtures.data.repository
 
+import androidx.lifecycle.asFlow
+import com.glebalekseevjk.premierleaguefixtures.data.local.dao.MatchInfoDao
+import com.glebalekseevjk.premierleaguefixtures.data.local.model.MatchInfoDbModel
 import com.glebalekseevjk.premierleaguefixtures.data.remote.MatchInfoService
 import com.glebalekseevjk.premierleaguefixtures.domain.entity.MatchInfo
-import com.glebalekseevjk.premierleaguefixtures.domain.entity.ResultStatus
 import com.glebalekseevjk.premierleaguefixtures.domain.entity.Result
+import com.glebalekseevjk.premierleaguefixtures.domain.entity.ResultStatus
+import com.glebalekseevjk.premierleaguefixtures.domain.mapper.Mapper
 import com.glebalekseevjk.premierleaguefixtures.domain.repository.MatchInfoRepository
 import com.glebalekseevjk.premierleaguefixtures.domain.repository.MatchInfoRepository.Companion.TOTAL_PER_PAGE
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import retrofit2.Response
 
 class MatchInfoRepositoryImpl(
-    private val matchInfoService: MatchInfoService
+    private val matchInfoService: MatchInfoService,
+    private val matchInfoDao: MatchInfoDao,
+    private val mapper: Mapper<MatchInfo, MatchInfoDbModel>
 ) : MatchInfoRepository {
-    // Эмуляция локального хранилища
-    private val _matchListLocalStore: MutableStateFlow<List<MatchInfo>> =
-        MutableStateFlow(emptyList())
 
     override fun getMatch(matchNumber: Int): Flow<MatchInfo?> =
-        _matchListLocalStore.map {
-            it.firstOrNull { it.matchNumber == matchNumber }
-        }
+        matchInfoDao.get(matchNumber).asFlow().map { it?.let { mapper.mapDbModelToItem(it) } }
 
     override fun getMatchListRangeForPage(page: Int): Flow<Result<List<MatchInfo>>> =
         getMatchListRangeForPageFromNetwork(page).map {
             when (it.status) {
                 ResultStatus.SUCCESS -> {
                     // добавляю с заменой существующих в хранилище. patch
-                    val currentLocalList = _matchListLocalStore.value.toMutableList()
                     val newList = it.data
-                    newList.forEach { matchInfo ->
-                        if (currentLocalList.contains(matchInfo)) {
-                            val old =
-                                currentLocalList.find { it.matchNumber == matchInfo.matchNumber }
-                            val indexOld = currentLocalList.indexOf(old)
-                            currentLocalList[indexOld] = matchInfo
-                        } else {
-                            currentLocalList.add(matchInfo)
-                        }
-                    }
-                    _matchListLocalStore.emit(currentLocalList)
-
-                    // Получение данных из локального хранилища. cache
-                    val list = getRangeListForPage(_matchListLocalStore.value, page)
-                    return@map Result(it.status, list)
+                    matchInfoDao.addAll(*newList.map { mapper.mapItemToDbModel(it) }.toTypedArray())
                 }
                 ResultStatus.LOADING -> return@map it
-                ResultStatus.FAILURE -> {
-                    val list = getRangeListForPage(_matchListLocalStore.value, page)
-                    return@map Result(it.status, list)
-                }
+                ResultStatus.FAILURE -> {}
             }
-        }
+
+            // Получение данных из локального хранилища. cache
+            val list = matchInfoDao.getPagedMatchInfoList(
+                TOTAL_PER_PAGE,
+                page * TOTAL_PER_PAGE - TOTAL_PER_PAGE
+            ).map { mapper.mapDbModelToItem(it) }
+
+            return@map Result(it.status, list)
+        }.flowOn(Dispatchers.IO)
 
     private fun getMatchListRangeForPageFromNetwork(page: Int): Flow<Result<List<MatchInfo>>> =
         flow {
