ea0538b1ed6f0c20af0dcfbb6d91f80e1ed54d0e
GlebAlekseev <glebalekseevjk@gmail.com>
2023-02-05 00:28:48 +0300

    added match detail view model tests

--- app/src/test/java/com/glebalekseevjk/premierleaguefixtures/ui/viewmodel/MatchDetailViewModelTest.kt
+++ app/src/test/java/com/glebalekseevjk/premierleaguefixtures/ui/viewmodel/MatchDetailViewModelTest.kt
@@ -1,16 +1,61 @@
 package com.glebalekseevjk.premierleaguefixtures.ui.viewmodel
 
-import org.junit.Assert.*
 
+import com.glebalekseevjk.premierleaguefixtures.domain.entity.MatchInfo
+import com.glebalekseevjk.premierleaguefixtures.domain.entity.Resource
+import com.glebalekseevjk.premierleaguefixtures.domain.interactor.MatchInfoUseCase
+import com.glebalekseevjk.premierleaguefixtures.ui.viewmodel.state.MatchDetailState
+import com.glebalekseevjk.premierleaguefixtures.utils.CoroutineRule
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class MatchDetailViewModelTest {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    var coroutinesTestRule = CoroutineRule()
 
-    @Test
-    fun getState() {
+    lateinit var viewModel: MatchDetailViewModel
+
+    @MockK
+    lateinit var matchInfoUseCase: MatchInfoUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        viewModel = MatchDetailViewModel(
+            matchInfoUseCase = matchInfoUseCase
+        )
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun setCurrentMatchInfo() {
+    fun `state should be set to Error`() = runTest {
+        coEvery {
+            matchInfoUseCase.getMatch(any())
+        } returns Resource.Failure(RuntimeException())
+
+        viewModel.setCurrentMatchInfo(1)
+
+        assertTrue(viewModel.state.value is MatchDetailState.Error<*>)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `state should be set to Loaded`() = runTest {
+        coEvery {
+            matchInfoUseCase.getMatch(any())
+        } returns Resource.Success(MatchInfo.MOCK)
+
+        viewModel.setCurrentMatchInfo(1)
+
+        assertTrue(viewModel.state.value is MatchDetailState.Loaded)
     }
 }
